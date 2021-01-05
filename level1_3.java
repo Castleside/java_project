e4e23a9489b27146627f60e30919c78f32b2406e
eliaskrtz <eliaskrautz@gmail.com>
2021-01-05 21:52:06 +0100

    tür in level1_3

--- level1_3.java
+++ level1_3.java
@@ -16,8 +16,7 @@ public class level1_3 extends level1
     public level1_3()
     {
         addObject(new mainChar(), 1190, 720/2);
-        addObject(new lv1_room3_1_door(60, 100), 1150, 30);        
-        
+        addObject(new lv1_room3_1_door(60, 100), 1150, 51);
         addObject(new lv1_3_knight(60,60), 673, 629);
         addObject(new lv1_3_knight(60,60), 673, 92);
         addObject(new lv1_3_knight(60,60), 451, 629);
@@ -35,17 +34,8 @@ public class level1_3 extends level1
         addObject(new lv1_3_candles(), 1009, 82);
         addObject(new lv1_3_candles(), 969, 82);
         addObject(new lv1_3_painting_1(), 562, 610);
-<<<<<<< HEAD
         addObject(new lv1_3_painting_2(), 784, 110);
         addObject(new lv1_3_painting_3(), 340, 610);
-        
-          
-=======
-        addObject(new lv1_3_painting_2(), 562, 110);
-        addObject(new lv1_3_painting_3(), 784, 610);
-        //addObject(new lv1_3_painting_4(), 784, 110);
->>>>>>> e135a61aaded57c57fe9e72789381410e39623a4
-        
         setPaintOrder(Doors.class, mainChar.class, solidObj.class);
     }
 }
--- lv1_room3_1_door.java
+++ lv1_room3_1_door.java
@@ -19,7 +19,7 @@ public class lv1_room3_1_door extends Doors
     
     public lv1_room3_1_door(int laenge, int breite) {
        //setImage("/backgrounds/redBG.jpg");
-       setRotation(90);
+       setRotation(0);
        GreenfootImage image = getImage();  
        //image.scale(laenge, breite);
        setImage(image);
