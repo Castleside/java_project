4ddce9f16a43592584faee4899f42a3663fd8aea
eliaskrtz <eliaskrautz@gmail.com>
2021-01-06 12:09:10 +0100

    nur ein Objekt nhemen - angefangen WIP

--- lv1_3_candles.java
+++ lv1_3_candles.java
@@ -12,6 +12,9 @@ public class lv1_3_candles extends lv1_3_object
      * Act - do whatever the lv1_3_candles wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public boolean crry = false;
+    
     public void act() 
     {
         List<mainChar>objects = getObjectsInRange(47, mainChar.class);
@@ -19,12 +22,13 @@ public class lv1_3_candles extends lv1_3_object
         mainChar actor = (mainChar)actors.get(0);
         int x = actor.getX() -45;
         int y = actor.getY();
-           
+      
      if(Greenfoot.isKeyDown("space")){
         if(objects.isEmpty() ){
             
         }
         else{
+           if (crry == false){
             if(Greenfoot.isKeyDown("w")){
             setLocation(x, y - 2 );
            }
@@ -38,15 +42,15 @@ public class lv1_3_candles extends lv1_3_object
             setLocation(x + 2, y  );
            }
         }
-           
-    }  
+    }
+    }else crry = false;
     }  
     
     public lv1_3_candles () {
         setImage("/level_1/candle.jpg");
-       setRotation(0);
-       GreenfootImage image = getImage();  
-       image.scale(20, 40);
-       setImage(image);
+        setRotation(0);
+        GreenfootImage image = getImage();  
+        image.scale(20, 40);
+        setImage(image);
     }
 }
