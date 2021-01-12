2ef02f060090668781bc7a9b3bab5b595893631e
JimmyDerbrecher <checkielp@gmail.com>
2021-01-12 11:26:42 +0100

    idk

--- level1_3.java
+++ level1_3.java
@@ -31,20 +31,14 @@ public class level1_3 extends level1
         addObject(new painting_5(), 340, 655);
         addObject(new painting_6(), 340, 65);
         addObject(new lv3_throne(), 200, 720/2);
-        addObject(new lv1_3_candles(), 1049, 82);
-        addObject(new lv1_3_candles(), 1009, 82);
-        addObject(new lv1_3_candles(), 969, 82);
+        addObject(new candle_1(), 1049, 82);
+        addObject(new candle_2(), 1009, 638);
+        addObject(new candle_3(), 969, 82);
         addObject(new lv1_3_painting_1(), 562, 610);
-<<<<<<< HEAD
         addObject(new lv1_3_painting_2(), 784, 110);
         addObject(new lv1_3_painting_3(), 340, 610);
-        
-          
-=======
-        addObject(new lv1_3_painting_2(), 562, 110);
-        addObject(new lv1_3_painting_3(), 784, 610);
-        //addObject(new lv1_3_painting_4(), 784, 110);
->>>>>>> e135a61aaded57c57fe9e72789381410e39623a4
+       
+
         
         setPaintOrder(Doors.class, mainChar.class, solidObj.class);
     }
--- lv1_3_candles.java
+++ lv1_3_candles.java
@@ -12,41 +12,22 @@ public class lv1_3_candles extends lv1_3_object
      * Act - do whatever the lv1_3_candles wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+ 
+    public static boolean place_1=false;
+    public static boolean place_2=false;
+    public static boolean place_3=false;
+    
     public void act() 
     {
-        List<mainChar>objects = getObjectsInRange(47, mainChar.class);
-        java.util.List actors = getWorld().getObjects(mainChar.class);
-        mainChar actor = (mainChar)actors.get(0);
-        int x = actor.getX() -45;
-        int y = actor.getY();
-           
-     if(Greenfoot.isKeyDown("space")){
-        if(objects.isEmpty() ){
-            
-        }
-        else{
-            if(Greenfoot.isKeyDown("w")){
-            setLocation(x, y - 2 );
-           }
-           if(Greenfoot.isKeyDown("s")){
-            setLocation(x, y + 2 );
-           }
-           if(Greenfoot.isKeyDown("a")){
-            setLocation(x - 2, y  );
-           }
-           if(Greenfoot.isKeyDown("d")){
-            setLocation(x + 2, y  );
-           }
-        }
-           
-    }  
+     if((place_1 && place_2) || (place_1 && place_3) || (place_2 && place_3)) {
+         candle_1 candle_1 = new candle_1();
+         candle_2 candle_2 = new candle_2();
+         candle_3 candle_3 = new candle_3();
+         candle_1.setLocation(1049, 82);
+         candle_2.setLocation(1009, 638);
+         candle_3.setLocation(969, 82);
+         
+    }
     }  
     
-    public lv1_3_candles () {
-        setImage("/level_1/candle.jpg");
-       setRotation(0);
-       GreenfootImage image = getImage();  
-       image.scale(20, 40);
-       setImage(image);
-    }
 }
