d4efa2e65560143dc59e09ed584b09f39a919646
JimmyDerbrecher <checkielp@gmail.com>
2021-02-01 14:36:21 +0100

    knight

---
+++ lv1_1_knight.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+/**
+ * Write a description of class lv1_1_knight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lv1_1_knight extends lv1_1
+{
+    /**
+     * Act - do whatever the lv1_1_knight wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public lv1_1_knight(){
+        setImage("knight.png");
+        setRotation(0);
+        GreenfootImage image = getImage();  
+        image.scale(58, 81);
+        setImage(image);
+    }
+    
+    public void act() 
+    {
+        List<mainChar>objects = getObjectsInRange(100, mainChar.class); 
+        if( objects.isEmpty()){
+          
+        } 
+        else{
+            if(Greenfoot.isKeyDown("e")){
+                World wrld = new lv1_1_hint();
+                Greenfoot.setWorld(wrld);
+            }
+        }
+    }
+    
+    
+    
+    
+}
